185129742c6ca472ad02279c25769d4c8fcb72b2
lichuang3 <lichuang3@tuhu.cn>
2025-02-06 11:42:51 +0800

    feat(controller): 添加 Excel 文件读取功能
    - 新增 ExcelReadController 类，实现 Excel 文件上传和读取功能
    - 添加处理 Excel 文件的相关依赖（apache.poi）
    -移除 HelloController 类

--- src/main/java/com/jungo/diy/controller/ExcelReadController.java
+++ src/main/java/com/jungo/diy/controller/ExcelReadController.java
@@ -1,10 +1,70 @@
 package com.jungo.diy.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * @author lichuang3
  * @date 2025-01-24 15:39
  */
+@RestController
+@Slf4j
 public class ExcelReadController {
 
+    @PostMapping("/upload-excel")
+    public List<List<String>> uploadExcel(@RequestParam("file") MultipartFile file) {
+        List<List<String>> data = new ArrayList<>();
+        try (InputStream inputStream = file.getInputStream();
+             Workbook workbook = new XSSFWorkbook(inputStream)) {
 
+            Sheet sheet = workbook.getSheetAt(0);
+            Iterator<Row> rowIterator = sheet.iterator();
+
+            while (rowIterator.hasNext()) {
+                Row row = rowIterator.next();
+                Iterator<Cell> cellIterator = row.cellIterator();
+                List<String> rowData = new ArrayList<>();
+
+                while (cellIterator.hasNext()) {
+                    Cell cell = cellIterator.next();
+                    switch (cell.getCellType()) {
+                        case STRING:
+                            rowData.add(cell.getStringCellValue());
+                            break;
+                        case NUMERIC:
+                            if (DateUtil.isCellDateFormatted(cell)) {
+                                rowData.add(cell.getDateCellValue().toString());
+                            } else {
+                                rowData.add(String.valueOf(cell.getNumericCellValue()));
+                            }
+                            break;
+                        case BOOLEAN:
+                            rowData.add(String.valueOf(cell.getBooleanCellValue()));
+                            break;
+                        case FORMULA:
+                            rowData.add(cell.getCellFormula());
+                            break;
+                        default:
+                            rowData.add("UNKNOWN");
+                            break;
+                    }
+                }
+                data.add(rowData);
+            }
+        } catch (IOException e) {
+            log.error("ExcelReadController#uploadExcel,出现异常！", e);
+        }
+        return data;
+    }
 }
