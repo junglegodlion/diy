b377b158b103a8158b8fc258fb83c5aa6aaa3c95
lichuang3 <lichuang3@tuhu.cn>
2025-02-07 15:55:43 +0800

    feat(export): 优化数据导出功能
    - 重构了 exportToExcel 方法，提高了代码的可维护性和可读性
    - 添加了日志记录，便于追踪异常信息
    - 在导出数据的同时，也在本地保存了一份备份
    - 优化了文件命名规则，增加了下划线以提高可读性
    - 使用 try-with-resources 语句自动管理资源，避免了手动关闭资源的繁琐操作

--- src/main/java/com/jungo/diy/service/ExportService.java
+++ src/main/java/com/jungo/diy/service/ExportService.java
@@ -1,14 +1,17 @@
 package com.jungo.diy.service;
 
 import com.jungo.diy.response.UrlPerformanceResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
@@ -17,6 +20,7 @@ import java.util.List;
  * @author lichuang3
  */
 @Service
+@Slf4j
 public class ExportService {
 
     public void exportToExcel(List<UrlPerformanceResponse> criticalLinkUrlPerformanceResponses,
@@ -27,38 +31,52 @@ public class ExportService {
                               List<UrlPerformanceResponse> accessVolumeTop30Interface,
                               HttpServletResponse response) throws IOException {
         // 创建工作簿
-        Workbook workbook = new XSSFWorkbook();
+        try (Workbook workbook = new XSSFWorkbook()) {
+            // 定义 Sheet 名称和数据列表
+            String[] sheetNames = {"关键链路", "五大金刚", "首屏Tab", "麒麟组件接口", "其他核心业务接口", "访问量top30接口"};
+            List<UrlPerformanceResponse>[] dataLists = new List[]{
+                    criticalLinkUrlPerformanceResponses,
+                    fiveGangJingUrlPerformanceResponses,
+                    firstScreenTabUrlPerformanceResponses,
+                    qilinComponentInterfaceUrlPerformanceResponses,
+                    otherCoreBusinessInterfaceUrlPerformanceResponses,
+                    accessVolumeTop30Interface
+            };
 
-        // 创建第一个 Sheet 并写入数据
-        createSheet(workbook, "关键链路", criticalLinkUrlPerformanceResponses);
-        // 创建第二个 Sheet 并写入数据
-        createSheet(workbook, "五大金刚", fiveGangJingUrlPerformanceResponses);
-        // 创建第三个 Sheet 并写入数据
-        createSheet(workbook, "首屏Tab", firstScreenTabUrlPerformanceResponses);
-        // 创建第四个 Sheet 并写入数据
-        createSheet(workbook, "麒麟组件接口", qilinComponentInterfaceUrlPerformanceResponses);
-        // 创建第五个 Sheet 并写入数据
-        createSheet(workbook, "其他核心业务接口", otherCoreBusinessInterfaceUrlPerformanceResponses);
-        // 创建第六个 Sheet 并写入数据
-        createSheet(workbook, "访问量top30接口", accessVolumeTop30Interface);
+            // 创建多个 Sheet 并写入数据
+            for (int i = 0; i < sheetNames.length; i++) {
+                createSheet(workbook, sheetNames[i], dataLists[i]);
+            }
 
-        // 设置响应头
-        response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
-        response.setCharacterEncoding("utf-8");
+            // 设置响应头
+            response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+            response.setCharacterEncoding("utf-8");
 
-        // 获取当天日期并格式化为 yyyy-MM-dd 格式
-        LocalDate currentDate = LocalDate.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        String formattedDate = currentDate.format(formatter);
-        String fileName = URLEncoder.encode(formattedDate + "performance.xlsx", "UTF-8");
-        response.setHeader("Content-disposition", "attachment;filename=" + fileName);
+            // 获取当天日期并格式化为 yyyy-MM-dd 格式
+            LocalDate currentDate = LocalDate.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            String formattedDate = currentDate.format(formatter);
+            String fileName = URLEncoder.encode(formattedDate + "_performance.xlsx", StandardCharsets.UTF_8.toString());
+            response.setHeader("Content-disposition", "attachment;filename=" + fileName);
 
-        // 输出文件
-        OutputStream outputStream = response.getOutputStream();
-        workbook.write(outputStream);
-        workbook.close();
-        outputStream.flush();
-        outputStream.close();
+            // 拼接完整的文件路径
+            String directoryPath = System.getProperty("user.home") + "/Desktop/备份/c端网关接口性能统计/数据统计/输出";
+            String filePath = directoryPath + "/" + fileName;
+
+            // 写入文件
+            try (FileOutputStream fileOut = new FileOutputStream(filePath)) {
+                workbook.write(fileOut);
+            } catch (IOException e) {
+                log.error("ExportService#exportToExcel,出现异常！", e);
+            }
+
+            // 输出文件
+            try (OutputStream outputStream = response.getOutputStream()) {
+                workbook.write(outputStream);
+            }
+        } catch (IOException e) {
+            log.error("ExportService#exportToExcel,出现异常！", e);
+        }
     }
 
     private void createSheet(Workbook workbook, String sheetName, List<UrlPerformanceResponse> data) {
