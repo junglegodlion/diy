3684c7da1dc796eb08bf0cd64d5a6398bbb0da12
lichuang3 <lichuang3@tuhu.cn>
2025-02-19 13:50:02 +0800

    refactor(diy): 优化日期格式转换
    - 将 Date 对象转换为 LocalDate 对象
    - 使用 DateTimeFormatter 将 LocalDate 对象格式化为字符串
    - 修改 P99Model 的日期格式为 "yyyy-MM-dd"
    - 移除了未使用的 sheetNames 数组

--- src/main/java/com/jungo/diy/service/AnalysisService.java
+++ src/main/java/com/jungo/diy/service/AnalysisService.java
@@ -9,9 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletResponse;
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
 
 import static com.jungo.diy.controller.FileReadController.createP99ModelSheet;
@@ -34,8 +38,6 @@ public class AnalysisService {
         List<P99Model> p99Models = getP99Models(apiDailyPerformanceEntities);
         // 画图
         try (XSSFWorkbook workbook = new XSSFWorkbook()) {
-            // 定义 Sheet 名称和数据列表
-            String[] sheetNames = {"99线", "周维度99线", "慢请求率", "周维度慢请求率"};
 
             createP99ModelSheet(workbook, "99线变化率", p99Models, "gateway 99线", "日期", "99线", "99线");
             // 6. 保存文件
@@ -50,7 +52,15 @@ public class AnalysisService {
         List<P99Model> p99Models = new ArrayList<>();
         for (ApiDailyPerformanceEntity apiDailyPerformanceEntity : apiDailyPerformanceEntities) {
             P99Model p99Model = new P99Model();
-            p99Model.setDate(apiDailyPerformanceEntity.getDate().toString());
+            Date date = apiDailyPerformanceEntity.getDate();
+            // 将 Date 对象转换为 LocalDate 对象
+            Instant instant = date.toInstant();
+            LocalDate localDate = instant.atZone(ZoneId.systemDefault()).toLocalDate();
+            // 定义日期格式
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            // 将 LocalDate 对象转换为字符串
+            String dateString = localDate.format(formatter);
+            p99Model.setDate(dateString);
             p99Model.setP99(apiDailyPerformanceEntity.getP99());
             p99Models.add(p99Model);
         }
