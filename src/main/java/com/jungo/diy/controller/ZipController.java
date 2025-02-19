857032bd168883c5fb8230cee659fba50639ab10
lichuang3 <lichuang3@tuhu.cn>
2025-02-19 09:48:51 +0800

    refactor(ZipController): 优化 ZIP 文件上传和解压功能- 添加 StringUtils 引用，提高代码可读性和健壮性- 增加对文件名为空的检查，提高代码安全性- 获取当前目录路径，动态生成解压目录 - 优化代码结构，增加注释，提高代码可读性

--- src/main/java/com/jungo/diy/controller/ZipController.java
+++ src/main/java/com/jungo/diy/controller/ZipController.java
@@ -1,5 +1,6 @@
 package com.jungo.diy.controller;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,9 +16,18 @@ import java.nio.file.Paths;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
+/**
+ * @author lichuang3
+ */
 @RestController
 public class ZipController {
-    
+
+    /**
+     * 处理 ZIP 文件上传并解压
+     *
+     * @param file 上传的 ZIP 文件
+     * @return 解压结果信息
+     */
     @PostMapping("/upload-zip")
     public ResponseEntity<String> handleZipUpload(@RequestParam("file") MultipartFile file) {
         try {
@@ -28,12 +38,14 @@ public class ZipController {
 
             // 检查文件扩展名是否为 .zip
             String fileName = file.getOriginalFilename();
-            if (!fileName.endsWith(".zip")) {
+            if (StringUtils.isBlank(fileName) || !fileName.endsWith(".zip")) {
                 return ResponseEntity.badRequest().body("请上传有效的 ZIP 文件");
             }
 
+            // 获得当前目录
+            String currentDirectory = System.getProperty("user.dir");
             // 创建解压目录
-            String outputFolder = "unzip/" + System.currentTimeMillis();
+            String outputFolder = currentDirectory + "/" + System.currentTimeMillis();
             Files.createDirectories(Paths.get(outputFolder));
 
             // 使用 GBK 编码处理 ZIP 文件
@@ -43,7 +55,8 @@ public class ZipController {
                 while ((entry = zis.getNextEntry()) != null) {
                     if (!entry.isDirectory()) {
                         String filePath = outputFolder + "/" + entry.getName();
-                        extractFile(zis, filePath); // 调用解压方法
+                        // 调用解压方法
+                        extractFile(zis, filePath);
                     }
                     zis.closeEntry();
                 }
