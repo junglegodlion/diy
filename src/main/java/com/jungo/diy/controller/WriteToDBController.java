11de48b4b1f9615c7c88e6d1abf419e50eb626eb
lichuang3 <lichuang3@tuhu.cn>
2025-03-20 15:04:29 +0800

    feat(controller): 为 WriteToDBController 添加 Swagger 注解
    - 在类级别添加 @Api 注解，指定控制器的描述
    - 在方法级别添加 @ApiOperation 注解，描述方法的功能和响应类型
    - 在参数级别添加 @ApiParam 注解，说明参数的用途和是否必填

--- src/main/java/com/jungo/diy/controller/WriteToDBController.java
+++ src/main/java/com/jungo/diy/controller/WriteToDBController.java
@@ -1,6 +1,10 @@
+
 package com.jungo.diy.controller;
 
 import com.jungo.diy.service.FileReaderService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,14 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * @author lichuang3
  */
+@Api(value = "文件操作控制器")
 @RestController
 @RequestMapping("/files")
 public class WriteToDBController {
     @Autowired
     private FileReaderService fileReaderService;
- 
+
+    @ApiOperation(value = "从指定目录获取文件", response = ResponseEntity.class)
     @GetMapping
-    public ResponseEntity<?> getFiles(@RequestParam("directoryName") String directoryName) {
+    public ResponseEntity<?> getFiles(@ApiParam(value = "目录名", required = true) @RequestParam("directoryName") String directoryName) {
         return ResponseEntity.ok(fileReaderService.readTargetFiles(directoryName));
     }
 }
