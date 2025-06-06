fbf4a9deeb6899586173a8482d9504ea83e5bffd
lichuang3 <lichuang3@tuhu.cn>
2025-06-06 16:25:29 +0800

    优化代码： 1. 添加测试数据文件src/main/resources/testdata/object 2. 修改ToStringParser2.java从文件读取测试数据 3. 增加文件存在性检查逻辑

--- src/main/java/com/jungo/diy/test/ToStringParser2.java
+++ src/main/java/com/jungo/diy/test/ToStringParser2.java
@@ -2,6 +2,9 @@ package com.jungo.diy.test;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -9,7 +12,12 @@ import java.util.regex.Pattern;
 public class ToStringParser2 {
 
     public static void main(String[] args) throws Exception {
-        String input = "BizResponse(code=10000, message=操作成功, data=ActivityPreviewResponse(unitPreviewInfos=[UnitPreviewInfo(activityId=3FLw07UdFK7ZO, versionInterval=null, unitId=2Vw1XqQ0YqdxO, unitType=MASTER_ORDER_AUDIT_UNIT, unitName=内容审核, unitDesc=, unitNameForC=途虎养车反馈有礼, unitBenefitPointForC=途虎反馈有礼, unitConditionDescForC=反馈体验 得途虎25元E卡, awardConfigs=[AwardConfig(unitAwardId=3QWmX9gKmh8dU, awardRuleGroup=3TXb2TMd3klKM, awardType=E_CARD, benefitId=3T3BiPdM9n0d8, awardValue=2500, grantMethod=AUTO, awardBenefitPointForC=途虎反馈有礼 得价值25元途虎E卡（支付直接抵扣）, awardBenefitPointUrlForC=null, awardBenefitPointUrlForPop=null, redeemTipsForC=null, awardContent={\"appId\": \"WXAPP\", \"channel\": \"activityUnit\", \"businessId\": \"activityUnit\", \"costCenter\": \"a\", \"financialAccount\": \"10296642\"})], hisRelatedEventIds=[950432637], activityPreviewInfo=ActivityPreviewInfo(activityId=3FLw07UdFK7ZO, activityType=5810, activityStartDate=Tue Apr 01 00:00:00 CST 2025, activityEndDate=Tue Jul 01 00:00:00 CST 2025, createdTime=Mon Mar 24 10:06:01 CST 2025, activityMaterialInfos=null, extraFieldMap={BIZ_SCENE=MARKETING, ACTIVITY_TEMPLATE=REPUTATION, ACTIVITY_RULE_URL=, ACTIVITY_NAME_FOR_C=途虎养车反馈有礼}))]))";
+        // 从文件读取测试数据
+        Path path = Paths.get("src/main/resources/testdata/object");
+        if(!Files.exists(path)) {
+            throw new IllegalStateException("Test data file not found: " + path);
+        }
+        String input = new String(Files.readAllBytes(path));
         Map<String, Object> jsonMap = parseToMap(input);
         ObjectMapper mapper = new ObjectMapper();
         String json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonMap);
