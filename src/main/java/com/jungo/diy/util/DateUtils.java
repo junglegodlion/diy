d4691ad7cb353bc5ce15a04b4e2c96125521621e
lichuang3 <lichuang3@tuhu.cn>
2025-06-19 18:17:23 +0800

    1. 完善DateUtils工具类的文档注释 2. 为所有公共方法添加详细的Javadoc说明 3. 补充getWeekNumber方法的实现说明 4. 增加日期字符串解析为LocalDate的方法文档

--- src/main/java/com/jungo/diy/util/DateUtils.java
+++ src/main/java/com/jungo/diy/util/DateUtils.java
@@ -8,6 +8,7 @@ import java.util.Calendar;
 import java.util.Date;
 
 /**
+ * 日期工具类，提供常用的日期格式化和转换方法
  * @author lichuang3
  * @date 2025-03-09 22:15
  */
@@ -19,23 +20,46 @@ public class DateUtils {
 
     private DateUtils() {}
 
+    /**
+     * 将LocalDate格式化为指定格式的字符串
+     * @param currentDate 要格式化的日期
+     * @param format 目标格式
+     * @return 格式化后的日期字符串
+     */
     public static String getDateString(LocalDate currentDate, String format) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(format);
         return currentDate.format(formatter);
     }
 
+    /**
+     * 获取Date对象对应的月份(1-12)
+     * @param date 日期对象
+     * @return 月份(1-12)
+     */
     public static int getMonth(Date date) {
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(date);
         return calendar.get(Calendar.MONTH) + 1;
     }
 
+    /**
+     * 获取LocalDate对应的ISO周数
+     * @param localDate 日期
+     * @return ISO标准周数
+     */
     public static int getWeekNumber(LocalDate localDate) {
         // 使用ISO周规则计算周数
+        // 使用ISO周规则计算周数(周一为一周的第一天)
         WeekFields weekFields = WeekFields.ISO;
         return localDate.get(weekFields.weekOfWeekBasedYear());
     }
 
+    /**
+     * 将Date对象格式化为指定格式的字符串
+     * @param date 要格式化的日期
+     * @param format 目标格式
+     * @return 格式化后的日期字符串
+     */
     public static String getDateString(Date date, String format) {
         LocalDate localDate = date.toInstant()
                 .atZone(ZoneId.systemDefault())
@@ -43,6 +67,12 @@ public class DateUtils {
         return localDate.format(DateTimeFormatter.ofPattern(format));
     }
 
+    /**
+     * 将字符串解析为LocalDate对象
+     * @param date 日期字符串
+     * @param format 日期格式
+     * @return 解析后的LocalDate对象
+     */
     public static LocalDate getLocalDate(String date, String format) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(format);
         return LocalDate.parse(date, formatter);
